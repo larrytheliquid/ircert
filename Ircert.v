2f807d16d93f8508a9f86f101970e8a4b0e511a1
Larry Diehl <larrytheliquid@gmail.com>
2011-08-19 16:24:17 -0700

    Start proofs

--- Ircert.v
+++ Ircert.v
@@ -1,10 +1,12 @@
 Require Import Arith String List Tactics.
+Require Import Tactics.
 Set Implicit Arguments.
 
 Definition User := nat.
 Definition usr_eq := eq_nat_dec.
 Definition Channel := nat.
 Definition chn_eq := eq_nat_dec.
+Definition str_eq := string_dec.
 (* TODO: channel name type, and join gets ERR_NOSUCHCHANNEL if it is the invalid constructor *)
 
 Inductive Command : Set :=
@@ -24,78 +26,61 @@ Inductive Response : Set :=
 | ERR_CANNOTSENDTOCHAN : User -> Channel -> Response
 .
 
+Definition rsp_eq (r1 r2:Response) : {r1=r2} + {r1<>r2}.
+  Hint Resolve usr_eq.
+  Hint Resolve chn_eq.
+  Hint Resolve str_eq.
+  decide equality.
+Defined.
+
 Definition Users := list User.
 Definition Responses := list Response.
 Definition State := list (Channel * Users).
 
-Fixpoint in_channel (usr:User) (xs:Users) : bool :=
+Fixpoint in_users (usr:User) (xs:Users) : bool :=
   match xs with
     | nil => false
     | x :: xs' => if usr_eq usr x
       then true
-      else in_channel usr xs'
+      else in_users usr xs'
   end.
 
-(* TODO: RPL_NAMEREPLY list to usr *)
-Fixpoint join_channel (usr:User) (chn:Channel) (xs:State) : Responses * State :=
-  let joined := fun usrs => (
-    map (fun x => EVN_JOIN x usr chn) (usr :: usrs) , 
-    (chn , usr :: usrs) :: xs
-  ) in
-
+Fixpoint members (chn:Channel) (xs:State) : Users :=
   match xs with
-    | nil => joined nil
+    | nil => nil
+    | (chn' , usrs) :: xs' => if chn_eq chn chn'
+      then usrs
+      else members chn xs'
+  end.
+
+Fixpoint in_channel (usr:User) (chn:Channel) (xs:State) : bool :=
+  in_users usr (members chn xs).
+
+Fixpoint join_channel (usr:User) (chn:Channel) (xs:State) : Responses :=
+  match xs with
+    | nil => EVN_JOIN usr usr chn :: nil
     | (chn' , usrs) :: xs' => if chn_eq chn chn' 
-      then if in_channel usr usrs 
-        then (nil , xs)
-        else joined usrs
+      then EVN_JOIN usr usr chn :: nil
       else
         match join_channel usr chn xs' with
-          | (es , xs'') => (es , (chn' , usrs) :: xs'')
+          | es => es
         end
   end.
 
-Fixpoint part_channel (usr:User) (chn:Channel) (xs:State) : Responses * State :=
-  match xs with
-    | nil => (ERR_NOSUCHCHANNEL usr chn :: nil , xs)
-    | (chn' , usrs) :: xs' => if chn_eq chn chn' 
-      then if in_channel usr usrs
-        then     
-          let usrs' := remove usr_eq usr usrs in (
-            map (fun x => EVN_PART x usr chn) usrs , 
-            (chn , usrs') :: xs'
-          )
-        else
-          (ERR_NOTONCHANNEL usr chn :: nil , xs)
-      else
-        match part_channel usr chn xs' with
-          | (es , xs'') => (es , (chn' , usrs) :: xs'')
-        end
-  end.
+Ltac ifs :=
+repeat (match goal with
+  | [ |- context[if ?x then _ else _] ] => destruct x
+end); auto.
 
-Fixpoint msg_channel (usr:User) (chn:Channel) (msg:string) (xs:State)
-  : Responses * State :=
-  match xs with
-    | nil => (ERR_NOSUCHNICK usr chn :: nil , xs)
-    | (chn' , usrs) :: xs' => if chn_eq chn chn' 
-      then if in_channel usr usrs
-        then
-          let usrs' := remove usr_eq usr usrs in (
-            map (fun x => EVN_MSG x usr chn msg) usrs' , 
-            xs
-          )
-        else (ERR_CANNOTSENDTOCHAN usr chn :: nil , xs)
-      else
-        match msg_channel usr chn msg xs' with
-          | (es , xs'') => (es , (chn' , usrs) :: xs'')
-        end
-  end.
+Ltac cases :=
+intros; match goal with
+  | [ x : State |- _ ] => induction x
+end; crush.
 
-Definition step (xs:State) (cmd:Command) : Responses * State :=
-  match cmd with
-    | JOIN usr chn => join_channel usr chn xs
-    | PART usr chn => part_channel usr chn xs
-    | PRIVMSG usr chn msg => msg_channel usr chn msg xs
-    (* | _ => (nil , nil) *)
-  end.
+Lemma fooooo usr chn xs :
+  join_channel usr chn xs = (EVN_JOIN usr usr chn) :: nil.
+cases; ifs.
+Qed.
+
+
 
