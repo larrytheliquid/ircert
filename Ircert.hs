14f3e64b2012124ed1588e0cd9df957a7b953f71
Larry Diehl <larrytheliquid@gmail.com>
2011-08-25 19:23:37 -0700

    start with more commands

--- Ircert.hs
+++ Ircert.hs
@@ -1,5 +1,8 @@
 module Ircert where
+import Data.List
 import Test.SmallCheck
+
+{- Types -}
 
 type User = Int
 type Channel = Int
@@ -11,11 +14,14 @@ data Command =
 
 data Response =
    Evn_Join User User Channel
+ | Evn_Part User User Channel
   deriving Eq
 
 type Users = [User]
 type Responses = [Response]
 type State = [(Channel , Users)]
+
+{- Predicates -}
 
 inUsers :: User -> Users -> Bool
 inUsers = elem
@@ -32,6 +38,8 @@ inChannel usr chn xs = inUsers usr (members chn xs)
 inResponses :: Response -> Responses -> Bool
 inResponses = elem
 
+{- Commands -}
+
 joinChannel :: User -> Channel -> State -> Responses
 joinChannel usr chn xs = map (\x -> Evn_Join x usr chn) (usr : members chn xs)
 
@@ -40,6 +48,22 @@ joinChannel' usr chn [] = [(chn , [usr])]
 joinChannel' usr chn ((chn' , usrs) : xs) = if chn == chn'
   then (chn' , usr : usrs) : xs
   else (chn' , usrs) : joinChannel' usr chn xs
+
+partChannel :: User -> Channel -> State -> Responses
+partChannel usr chn xs = map (\x -> Evn_Part x usr chn) (usr : members chn xs)
+
+partChannel' :: User -> Channel -> State -> State
+partChannel' _ _ [] = []
+partChannel' usr chn ((chn' , usrs) : xs) = if chn == chn'
+  then (chn' , delete usr usrs) : xs
+  else (chn' , usrs) : partChannel' usr chn xs
+
+respond :: Command -> State -> (Responses , State)
+respond (Join usr chn) xs = (joinChannel usr chn xs , joinChannel' usr chn xs)
+respond (Part _ _) _ = undefined
+respond (Privmsg _ _ _) _ = undefined
+
+{- Properties -}
 
 prop_joiner_gets_message :: User -> Channel -> Users -> Bool
 prop_joiner_gets_message usr chn usrs =
@@ -55,6 +79,7 @@ prop_all_members_get_join_message usr joiner chn xs =
   inChannel usr chn xs' ==>
   inResponses (Evn_Join usr joiner chn) (joinChannel joiner chn xs')
 
+main :: IO ()
 main = do
   smallCheck 4 prop_joiner_gets_message
   smallCheck 3 prop_join_implies_in_channel
