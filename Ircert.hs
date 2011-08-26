cf6ca6f834c7bc2a00104c76ec05f2edf5045e29
Larry Diehl <larrytheliquid@gmail.com>
2011-08-25 17:00:28 -0700

    some props

--- Ircert.hs
+++ Ircert.hs
@@ -1,4 +1,5 @@
 module Ircert where
+import Test.QuickCheck
 
 type User = Int
 type Channel = Int
@@ -34,3 +35,22 @@ inResponses = elem
 joinChannel :: User -> Channel -> State -> Responses
 joinChannel usr chn xs = map (\x -> Evn_Join x usr chn) (usr : members chn xs)
 
+joinChannel' :: User -> Channel -> State -> State
+joinChannel' usr chn [] = [(chn , [usr])]
+joinChannel' usr chn ((chn' , usrs) : xs) = if chn == chn'
+  then (chn' , usr : usrs) : xs
+  else (chn' , usrs) : joinChannel' usr chn xs
+
+prop_joiner_gets_message :: User -> Channel -> Users -> Bool
+prop_joiner_gets_message usr chn usrs =
+  inResponses (Evn_Join usr usr chn) (map (\x -> Evn_Join x usr chn) (usr : usrs))
+
+prop_all_members_get_join_message :: User -> User -> Channel -> State -> Property
+prop_all_members_get_join_message usr joiner chn xs =
+  inChannel usr chn xs ==>
+  inResponses (Evn_Join usr joiner chn) (joinChannel joiner chn xs)
+
+main = do
+  quickCheck prop_joiner_gets_message
+  quickCheck prop_all_members_get_join_message
+  putStrLn "Done!"
